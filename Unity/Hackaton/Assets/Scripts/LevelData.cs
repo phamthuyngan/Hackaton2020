3d0298313708b7f2435d491c24da8a1cbf88afe0
= <=>
2020-11-26 11:11:46 +0100

    Export JSON file

---
+++ Unity/Hackaton/Assets/Scripts/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class LevelData : MonoBehaviour
+{
+    public string pseudo = "Hello World";
+    public int cycles = 50;
+    public string date = DateTime.Now.ToString();
+}
---
+++ Unity/Hackaton/Assets/Scripts/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(LevelData))]
+public class UIManager : MonoBehaviour
+{
+    private GameManager gameManager;
+    private LevelData lvlData;
+    void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+        lvlData = GetComponent<LevelData>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(lvlData);
+        gameManager.ExportData(data);
+    }
+
+}
