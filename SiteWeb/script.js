5dcbdac28e96cde703da7d0f61ceb4c80fca0ada
Katarina Dordevic <katarina@Katarinas-MBP.lan>
2020-11-26 14:24:46 +0100

    sorting high scores in js

--- SiteWeb/script.js
+++ SiteWeb/script.js
@@ -47,18 +47,43 @@ script.onload = () => {
 };
 document.body.appendChild(script);
 
+// script for the site
+// getting info from json file:
+
 $.get("http://localhost:3000/joueurs").then((data) => {
   console.table(data);
-  for (let i = 0; i < $(data).length; i++) {
+  // finding the last player:
+  const player = $(
+    `<tr><td>${data[data.length - 1].pseudo}</td><td>${
+      data[data.length - 1].score
+    }</td></tr> `
+  );
+  $(".player").append(player);
+
+  console.log(data[data.length - 1]);
+
+  // sorting the data by highest score
+  function sortResults(prop, asc) {
+    data.sort((a, b) => {
+      if (asc) {
+        return a[prop] > b[prop] ? 1 : a[prop] < b[prop] ? -1 : 0;
+      }
+      return b[prop] > a[prop] ? 1 : b[prop] < a[prop] ? -1 : 0;
+    });
+  }
+  sortResults("score", false);
+  console.table(data);
+  // showing the first five places
+  for (let i = 0; i < 5; i++) {
     const table = $(
       `<tr>
+       <td>${i + 1}</td>
         <td>${data[i].pseudo}</td>
         <td>${data[i].score}</td>
-        <td>${data[i].score}</td>
-      </tr>
-      `
+       
+      </tr>`
     );
 
-    $("table").append(table);
+    $(".score").append(table);
   }
 });
