ae7dc32e1e47a773df7ab467e16f7d541622ad09
katanais <nanavdh@hotmail.com>
2020-11-26 14:47:43 +0100

    deuxieme essai sorry

---
+++ Unity/Hackaton/Assets/ScriptMenu/Button.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public string sceneName;
+
+    void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
